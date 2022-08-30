1fd71b86d2396d888a5de4af1f7aed05597a7dfe
abdoul20 <abdoulkarimfoly1996@gmail.com>
2022-08-30 10:26:30 +0000

    changement des redirection pour la deconnexion

--- src/app/front-office/menu/menu.component.ts
+++ src/app/front-office/menu/menu.component.ts
@@ -18,6 +18,6 @@ export class MenuComponent implements OnInit {
   deconnexion(){
     this.loginService.logout();
     // this.user=null
-    this.routerRedirect.navigate(['/login']);
+    this.routerRedirect.navigate(['/']);
   }
 }
